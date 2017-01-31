da9d46f463c9663026d86bfac4d16bcef8bd5ebe
gffuma <fumagalli.gf@gmail.com>
2017-01-31 15:50:58 +0100

    Remove useless mapping in api

--- tag-aid/src/api/index.js
+++ tag-aid/src/api/index.js
@@ -12,17 +12,6 @@ const TEXT_ID = 'aeb7d4b8-15af-4cb5-8c87-fa38661943a0'
 
 export const getGraph = (start, end) =>
   fetchJson(`/stemmarest/tradition/${TEXT_ID}/subgraph/${start}/${end}`).then(({ json }) => json)
-  .then(graph => ({
-    ...graph,
-    relationships: graph.relationships.map(link => {
-      const swappedLink = {
-        ...link,
-        // source: link.target,
-        // target: link.source
-      }
-      return swappedLink
-    })
-  }))
 
 export const searchText = (text) =>
   fetchJson(`/stemmarest/tradition/${TEXT_ID}/search/${text}`)
